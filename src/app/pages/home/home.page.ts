de9728acb3e83d55a586aa2b813184878fd4232b
Reginaldo do Prado <reginaldodoprado@gmail.com>
2019-11-10 18:51:33 -0300

    Cria a tela inicial

---
+++ src/app/pages/home/home.page.ts
@@ -0,0 +1,26 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-home',
+    templateUrl: 'home.page.html',
+    styleUrls: ['home.page.scss'],
+})
+export class HomePage {
+    showFab = false;
+    tituloPagina;
+    listaAvaliacao = [];
+
+    constructor() { }
+
+    editaAvaliacao(item) {
+
+    }
+
+    listaRelatorio(item) {
+
+    }
+
+    novaAvaliacao() {
+
+    }
+}
